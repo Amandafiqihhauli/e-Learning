37b7fb71b65018e2a12f77e8a7566dc0929777b4
Amandafiqihhauli <piki.cou@gmail.com>
2024-07-01 15:21:42 +0700

    praktikum 14

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.main')
+@section('content')
+    <h1>Anda Tidak Memiliki Hak Akses</h1>
+    <h2>401</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -54,37 +54,37 @@ Route::middleware('auth')->group(function () {
 
     // STUDENT
     // route untuk menampilkan form data student
-    Route::get('admin/student/create', [StudentController::class, 'create']);
+    Route::get('admin/student/create', [StudentController::class, 'create'])->middleware('admin');
 
     // route untuk mengirim data student
     Route::post('admin/student/store', [StudentController::class, 'store']);
 
 
     // Route untuk menampilkan halaman edit
-    Route::get('admin/student/edit/{id}', [StudentController::class, 'edit']);
+    Route::get('admin/student/edit/{id}', [StudentController::class, 'edit'])->middleware('admin');
 
     // route untuk menyimpan update student
     Route::put('admin/student/update/{id}', [StudentController::class, 'update']);
 
     // route untuk menghapus data
-    Route::DELETE('admin/student/delete/{id}', [StudentController::class, 'destroy']);
+    Route::DELETE('admin/student/delete/{id}', [StudentController::class, 'destroy'])->middleware('admin');
 
     // COURSES
     // route untuk menampilkan form data student
-    Route::get('admin/courses/create', [CoursesController::class, 'create']);
+    Route::get('admin/courses/create', [CoursesController::class, 'create'])->middleware('admin');
 
     // route untuk mengirim data student
     Route::post('admin/courses/store', [CoursesController::class, 'store']);
 
 
     // Route untuk menampilkan halaman edit
-    Route::get('admin/courses/edit/{id}', [CoursesController::class, 'edit']);
+    Route::get('admin/courses/edit/{id}', [CoursesController::class, 'edit'])->middleware('admin');
 
     // route untuk menyimpan update student
     Route::put('admin/courses/update/{id}', [CoursesController::class, 'update']);
 
     // route untuk menghapus data
-    Route::DELETE('admin/courses/delete/{id}', [CoursesController::class, 'destroy']);
+    Route::DELETE('admin/courses/delete/{id}', [CoursesController::class, 'destroy'])->middleware('admin');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
